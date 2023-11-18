a4b3f295404f23ea6ef35b0cebb6860a6d48a337
Simone Rubino <daemo00@gmail.com>
2023-11-19 00:43:54 +0100

    Template update

--- src/MyCompilerInterpreter/cli.py
+++ src/MyCompilerInterpreter/cli.py
@@ -8,7 +8,7 @@ def parse_args(args=None):
     """Parse arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'code',
+        "code",
         type=str,
     )
     return parser.parse_args(args=args)
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,5 @@ from MyCompilerInterpreter.main import add_one
 
 
 class TestMain(unittest.TestCase):
-
     def test_add_one(self):
         self.assertEqual(add_one(5), 6)
